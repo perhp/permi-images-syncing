cebec972221a9f68efc0bba2764bf75803715e1f
Per Hansen <me@perhp.com>
2024-01-03 22:46:15 +0100

    sync all pass images

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ console.log('Starting up raspinoaa supabase syncing...');
 
 require('dotenv').config();
 
-import { readdir } from 'node:fs/promises';
+import { readFile, readdir } from 'node:fs/promises';
 import { supabase } from "./libs/supabase";
 import { DecodedPass } from './models/decoded-pass';
 
@@ -12,39 +12,40 @@ const db = new sqlite3.Database('/home/leducia/raspberry-noaa-v2/db/panel.db');
 (async () => {
     const images = (await readdir('/srv/images')).filter(path => path !== 'thumb');
     const thumbs = await readdir('/srv/images/thumb');
-    
+
     db.serialize(() => {
         db.each("SELECT * FROM decoded_passes", async (err: any, row: DecodedPass) => {
             if (err) {
                 console.error(err);
                 return;
             }
-    
-            const { id, file_path } = row;
-            const passThumbs = thumbs.filter(thumb => thumb.startsWith(file_path));
-            const passImages = images.filter(image => image.startsWith(file_path));
-            
+
+            const { id, file_path, pass_start } = row;
             const { data: existingPass, error: existingError } = await supabase.from('passes').select('id').eq('id', id).single();
             if (existingPass) {
                 console.warn(`Pass ${id} already exists`);
                 return;
             }
 
-            const { data: pass, error } = await supabase.from('passes').insert({
-                id, 
-                gain: row.gain, 
-                pass_start: row.pass_start, 
+            await supabase.from('passes').insert({
+                id,
+                gain: row.gain,
+                pass_start: new Date(row.pass_start * 1000),
                 daylight_pass: Boolean(row.daylight_pass),
-                has_histogram: Boolean(row.has_histogram), 
-                has_polar_az_el: Boolean(row.has_polar_az_el), 
-                has_polar_direction: Boolean(row.has_polar_direction), 
-                has_pristine: Boolean(row.has_pristine), 
+                has_histogram: Boolean(row.has_histogram),
+                has_polar_az_el: Boolean(row.has_polar_az_el),
+                has_polar_direction: Boolean(row.has_polar_direction),
+                has_pristine: Boolean(row.has_pristine),
                 has_spectrogram: Boolean(row.has_spectrogram),
                 is_noaa: file_path.includes('NOAA'),
                 is_meteor: file_path.includes('METEOR'),
             });
 
-            console.log('2:', error);
-        });        
+            const passImages = images.filter(image => image.startsWith(file_path));
+            await Promise.all([
+                ...passImages.map(image => supabase.from('passes_images').insert({ path: image, fk_passes_id: id })),
+                ...passImages.map(async image => supabase.storage.from('passes').upload(`images/${image}`, (await readFile(`/srv/images/${image}`)), { contentType: 'image/' + image.split('.').pop() })),
+            ]);
+        });
     });
 })();
