f495ed86919f3c7e2a7dbf44b73c214e6c313eba
Per Hansen <me@perhp.com>
2024-01-04 00:11:03 +0100

    use better sqlite3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-console.log('Starting up raspinoaa supabase syncing...');
+console.log('Starting up...');
 
 require('dotenv').config();
 
@@ -6,47 +6,49 @@ import { readFile, readdir } from 'node:fs/promises';
 import { supabase } from "./libs/supabase";
 import { DecodedPass } from './models/decoded-pass';
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('/home/leducia/raspberry-noaa-v2/db/panel.db');
+import Database from 'better-sqlite3';
+const db = new Database('/home/leducia/raspberry-noaa-v2/db/panel.db');
 
 async function sync() {
+    console.log('Syncing...');
+
     const images = (await readdir('/srv/images')).filter(path => path !== 'thumb');
+    const statement = db.prepare<DecodedPass[]>('SELECT * FROM decoded_passes');
+    const passes = statement.all();
+    for (const pass of passes as DecodedPass[]) {
+        console.log('    Syncing pass: ' + pass.id);
 
-    db.serialize(() => {
-        db.each("SELECT * FROM decoded_passes", async (err: any, row: DecodedPass) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
+        const { id } = pass as DecodedPass;
+        const { data: existingPass } = await supabase.from('passes').select('id').eq('id', id).single();
+        if (existingPass) {
+            console.warn(`    Pass ${id} already exists`);
+            continue;
+        }
 
-            const { id, file_path } = row;
-            const { data: existingPass } = await supabase.from('passes').select('id').eq('id', id).single();
-            if (existingPass) {
-                console.warn(`Pass ${id} already exists`);
-                return;
-            }
-
-            await supabase.from('passes').insert({
-                id,
-                gain: row.gain,
-                pass_start: new Date(row.pass_start * 1000),
-                daylight_pass: Boolean(row.daylight_pass),
-                has_histogram: Boolean(row.has_histogram),
-                has_polar_az_el: Boolean(row.has_polar_az_el),
-                has_polar_direction: Boolean(row.has_polar_direction),
-                has_pristine: Boolean(row.has_pristine),
-                has_spectrogram: Boolean(row.has_spectrogram),
-                is_noaa: file_path.includes('NOAA'),
-                is_meteor: file_path.includes('METEOR'),
-            });
-
-            const passImages = images.filter(image => image.startsWith(file_path));
-            await Promise.all([
-                ...passImages.map(image => supabase.from('passes_images').insert({ path: image, fk_passes_id: id })),
-                ...passImages.map(async image => supabase.storage.from('passes').upload(`images/${image}`, (await readFile(`/srv/images/${image}`)), { contentType: 'image/' + image.split('.').pop() })),
-            ]);
+        await supabase.from('passes').insert({
+            id,
+            gain: pass.gain,
+            pass_start: new Date(pass.pass_start * 1000),
+            daylight_pass: Boolean(pass.daylight_pass),
+            has_histogram: Boolean(pass.has_histogram),
+            has_polar_az_el: Boolean(pass.has_polar_az_el),
+            has_polar_direction: Boolean(pass.has_polar_direction),
+            has_pristine: Boolean(pass.has_pristine),
+            has_spectrogram: Boolean(pass.has_spectrogram),
+            is_noaa: pass.file_path.includes('NOAA'),
+            is_meteor: pass.file_path.includes('METEOR'),
         });
-    });
+
+        const passImages = images.filter(image => image.startsWith(pass.file_path));
+        await Promise.all([
+            ...passImages.map(image => supabase.from('passes_images').insert({ path: image, fk_passes_id: id })),
+            ...passImages.map(async image => supabase.storage.from('passes').upload(`images/${image}`, (await readFile(`/srv/images/${image}`)), { contentType: 'image/' + image.split('.').pop() })),
+        ]);
+
+        console.log(`    Pass ${id} synced succesfully`);
+    }
+
+    console.log('Done!');
 }
 
 // Sync every 30 minutes
