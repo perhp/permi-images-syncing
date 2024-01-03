e4a82e50828d38aa5ce4ddbf90a0e08422b601bd
Per Hansen <me@perhp.com>
2024-01-03 19:45:13 +0100

    sync data to supabase

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
+console.log('Starting up raspinoaa supabase syncing...');
+
 require('dotenv').config();
 
+import { readdir } from 'node:fs/promises';
 import { supabase } from "./libs/supabase";
 import { DecodedPass } from './models/decoded-pass';
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('/home/leducia/raspberry-noaa-v2/db/panel.db');
 
-db.serialize(() => {
-    db.each("SELECT * FROM decoded_passes", (err: any, row: DecodedPass) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+(async () => {
+    const images = (await readdir('/srv/images')).filter(path => path !== 'thumb');
+    const thumbs = await readdir('/srv/images/thumb');
+    
+    db.serialize(() => {
+        db.each("SELECT * FROM decoded_passes", async (err: any, row: DecodedPass) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+    
+            const { id, file_path } = row;
+            const passThumbs = thumbs.filter(thumb => thumb.startsWith(file_path));
+            const passImages = images.filter(image => image.startsWith(file_path));
+            
+            const { data: existingPass, error: existingError } = await supabase.from('passes').select('id').eq('id', id).single();
+            if (existingPass) {
+                console.warn(`Pass ${id} already exists`);
+                return;
+            }
 
-        console.log(row);
+            const { data: pass, error } = await supabase.from('passes').insert({
+                id, 
+                gain: row.gain, 
+                pass_start: row.pass_start, 
+                daylight_pass: Boolean(row.daylight_pass),
+                has_histogram: Boolean(row.has_histogram), 
+                has_polar_az_el: Boolean(row.has_polar_az_el), 
+                has_polar_direction: Boolean(row.has_polar_direction), 
+                has_pristine: Boolean(row.has_pristine), 
+                has_spectrogram: Boolean(row.has_spectrogram),
+                is_noaa: file_path.includes('NOAA'),
+                is_meteor: file_path.includes('METEOR'),
+            });
+
+            console.log('2:', error);
+        });        
     });
-});
+})();
--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = 'https://htyouocguzgigopxxidt.supabase.co';
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY as string;
-console.log(supabaseKey);
 export const supabase = createClient(supabaseUrl, supabaseKey);
