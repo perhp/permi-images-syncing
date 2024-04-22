4842d0fe2e976573e6132a12ec4eeb81a708cb94
Per Hansen <me@perhp.com>
2024-04-22 20:19:00 +0200

    feat: add new selected properties

--- src/models/decoded-pass.ts
+++ src/models/decoded-pass.ts
@@ -1,15 +1,20 @@
 export interface DecodedPass {
-    id: number;
-    pass_start: number;
-    file_path: string;
-    daylight_pass: number;
-    is_noaa: number | null;
-    sat_type: number;
-    img_count: number | null;
-    has_spectrogram: number;
-    has_pristine: number;
-    gain: number;
-    has_polar_az_el: number;
-    has_polar_direction: number;
-    has_histogram: number;
+  id: number;
+  pass_start: number;
+  file_path: string;
+  daylight_pass: number;
+  is_noaa: number | null;
+  sat_type: number;
+  img_count: number | null;
+  has_spectrogram: number;
+  has_pristine: number;
+  gain: number;
+  has_polar_az_el: number;
+  has_polar_direction: number;
+  has_histogram: number;
+  max_elev: number;
+  direction: string;
+  azimuth_at_max: number;
+  pass_end: number;
+  pass_start_azimuth: number;
 }
