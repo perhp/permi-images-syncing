4aa66e7f2e6932fd4f4bc3d60e61cfed65ce8460
Per Hansen <me@perhp.com>
2024-04-24 00:00:59 +0200

    feat: dont spam every pass that already exists

--- src/index.ts
+++ src/index.ts
@@ -31,107 +31,115 @@ async function sync() {
     (path) => path !== "thumb"
   );
   const statement = db.prepare<DecodedPass[]>(decodedPassesQuery);
-  const passes = statement.all();
-  for (const pass of passes as DecodedPass[]) {
-    console.log("    Syncing pass: " + pass.id);
+  const passes = statement.all() as DecodedPass[];
 
-    if (syncedPassesIds.includes(pass.id)) {
-      console.log("    - Pass already synced ¹\n");
-      continue;
-    }
+  if (passes.every((pass) => syncedPassesIds.includes(pass.id))) {
+    console.log("    No new passes to sync\n");
+  } else {
+    for (const pass of passes) {
+      if (syncedPassesIds.includes(pass.id)) {
+        continue;
+      }
 
-    console.log("    - Checking pass existence...");
-    const { data: existingPass } = await supabase
-      .from("passes")
-      .select("id")
-      .eq("id", pass.id)
-      .single();
-    if (existingPass) {
-      syncedPassesIds.push(pass.id);
-      console.warn("    - Pass already exists ²\n");
-      continue;
-    }
-
-    console.log("    - Inserting pass...");
-    const { error: passError } = await supabase.from("passes").insert({
-      id: pass.id,
-      gain: pass.gain,
-      pass_start: new Date(pass.pass_start * 1000),
-      daylight_pass: Boolean(pass.daylight_pass),
-      has_histogram: Boolean(pass.has_histogram),
-      has_polar_az_el: Boolean(pass.has_polar_az_el),
-      has_polar_direction: Boolean(pass.has_polar_direction),
-      has_pristine: Boolean(pass.has_pristine),
-      has_spectrogram: Boolean(pass.has_spectrogram),
-      max_elevation: pass.max_elev,
-      direction: pass.direction,
-      azimuth_at_max: pass.azimuth_at_max,
-      pass_end: new Date(pass.pass_end * 1000),
-      pass_start_azimuth: pass.pass_start_azimuth,
-      is_noaa: pass.file_path.includes("NOAA"),
-      is_meteor: pass.file_path.includes("METEOR"),
-    });
-
-    if (passError) {
-      console.warn(`    - Couldn't insert pass`);
-      console.log(JSON.stringify(passError, null, 2));
-      continue;
-    }
-
-    console.log("    - Uploading images...");
-    const passImages = images.filter((image) =>
-      image.startsWith(pass.file_path)
-    );
-    const imagesResponses:
-      | (
-          | {
-              data: {
-                path: string;
-              };
-              error: null;
-            }
-          | {
-              data: null;
-              error: any;
-            }
-          | PostgrestSingleResponse<null>
-        )[] = [];
-
-    let uploadCount = 1;
-    for (let image of passImages) {
-      const dbResponse = await supabase
-        .from("passes_images")
-        .insert({ path: image, fk_passes_id: pass.id });
-      const storageResponse = await supabase.storage
+      console.log("    Syncing pass: " + pass.id);
+      console.log("    - Checking pass existence...");
+      const { data: existingPass } = await supabase
         .from("passes")
-        .upload(`images/${image}`, await readFile(`/srv/images/${image}`), {
-          contentType: "image/" + image.split(".").pop(),
-          upsert: true,
-        });
+        .select("id")
+        .eq("id", pass.id)
+        .single();
+      if (existingPass) {
+        syncedPassesIds.push(pass.id);
+        console.warn("    - Pass already exists \n");
+        continue;
+      }
 
-      imagesResponses.push(dbResponse);
-      imagesResponses.push(storageResponse);
+      console.log("    - Inserting pass...");
+      const { error: passError } = await supabase.from("passes").insert({
+        id: pass.id,
+        gain: pass.gain,
+        pass_start: new Date(pass.pass_start * 1000),
+        daylight_pass: Boolean(pass.daylight_pass),
+        has_histogram: Boolean(pass.has_histogram),
+        has_polar_az_el: Boolean(pass.has_polar_az_el),
+        has_polar_direction: Boolean(pass.has_polar_direction),
+        has_pristine: Boolean(pass.has_pristine),
+        has_spectrogram: Boolean(pass.has_spectrogram),
+        max_elevation: pass.max_elev,
+        direction: pass.direction,
+        azimuth_at_max: pass.azimuth_at_max,
+        pass_end: new Date(pass.pass_end * 1000),
+        pass_start_azimuth: pass.pass_start_azimuth,
+        is_noaa: pass.file_path.includes("NOAA"),
+        is_meteor: pass.file_path.includes("METEOR"),
+      });
 
-      console.log(`    - ${uploadCount}/${passImages.length} images uploaded`);
+      if (passError) {
+        console.warn(`    - Couldn't insert pass`);
+        console.log(JSON.stringify(passError, null, 2));
+        continue;
+      }
 
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      uploadCount++;
+      console.log("    - Uploading images...");
+      const passImages = images.filter((image) =>
+        image.startsWith(pass.file_path)
+      );
+      const imagesResponses:
+        | (
+            | {
+                data: {
+                  path: string;
+                };
+                error: null;
+              }
+            | {
+                data: null;
+                error: any;
+              }
+            | PostgrestSingleResponse<null>
+          )[] = [];
+
+      let uploadCount = 1;
+      for (let image of passImages) {
+        const dbResponse = await supabase
+          .from("passes_images")
+          .insert({ path: image, fk_passes_id: pass.id });
+        const storageResponse = await supabase.storage
+          .from("passes")
+          .upload(`images/${image}`, await readFile(`/srv/images/${image}`), {
+            contentType: "image/" + image.split(".").pop(),
+            upsert: true,
+          });
+
+        imagesResponses.push(dbResponse);
+        imagesResponses.push(storageResponse);
+
+        console.log(
+          `    - ${uploadCount}/${passImages.length} images uploaded`
+        );
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        uploadCount++;
+      }
+
+      const imagesErrors = imagesResponses.filter(
+        (response) => response.error !== null
+      );
+      if (imagesErrors.length > 0) {
+        console.warn(`    - Couldn't upload all images`);
+        console.log(JSON.stringify(imagesErrors, null, 2));
+
+        await supabase.from("passes").delete().eq("id", pass.id);
+        await supabase
+          .from("passes_images")
+          .delete()
+          .eq("fk_passes_id", pass.id);
+        continue;
+      }
+
+      syncedPassesIds.push(pass.id);
+      console.log(`    - Pass synced succesfully\n`);
     }
-
-    const imagesErrors = imagesResponses.filter(
-      (response) => response.error !== null
-    );
-    if (imagesErrors.length > 0) {
-      console.warn(`    - Couldn't upload all images`);
-      console.log(JSON.stringify(imagesErrors, null, 2));
-
-      await supabase.from("passes").delete().eq("id", pass.id);
-      await supabase.from("passes_images").delete().eq("fk_passes_id", pass.id);
-      continue;
-    }
-
-    syncedPassesIds.push(pass.id);
-    console.log(`    - Pass synced succesfully\n`);
   }
 
   console.log(
