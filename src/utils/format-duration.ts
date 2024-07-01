8a06295df9d4bd1d49b0521fd5d68d1e482df0cc
Per Hansen <me@perhp.com>
2024-07-01 23:01:10 +0200

    refactor: use shortnames instead

--- src/utils/format-duration.ts
+++ src/utils/format-duration.ts
@@ -4,15 +4,15 @@ export const formatDuration = (ms: number) => {
   }
 
   const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
-    second: Math.floor(ms / 1000) % 60,
-    millisecond: Math.floor(ms) % 1000,
+    d: Math.floor(ms / 86400000),
+    h: Math.floor(ms / 3600000) % 24,
+    m: Math.floor(ms / 60000) % 60,
+    s: Math.floor(ms / 1000) % 60,
+    ms: Math.floor(ms) % 1000,
   };
 
   return Object.entries(time)
     .filter((val) => val[1] !== 0)
-    .map(([key, val]) => `${val} ${key}${val !== 1 ? "s" : ""}`)
+    .map(([key, val]) => `${val}${key}${val !== 1 ? "s" : ""}`)
     .join(", ");
 };
